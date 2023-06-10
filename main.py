ed2640205701b036208d46b8bef66870d3399b56
VassilinaPap <vassilinapapadouli@gmail.com>
2023-06-10 13:02:18 +0200

    Save Image

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ if __name__ == '__main__':
     camera_settings = pytorch_camera(params['rendering.rgb.image_size'], torch.from_numpy(viewpoints["K"]).float().to(device))
 
     writer = imageio.get_writer("./data/render.gif", mode='I', duration=0.3)
-    images = None
+    images = []
+    max_val = 0
     for i in range(n_images):
         prd_image = render_mesh(mesh, 
                                 modes='image_ct', #######
@@ -65,13 +66,13 @@ if __name__ == '__main__':
                                 device=device, background_colors=None, light_poses=None, materials=None, camera_settings=camera_settings,
                                 sigma=params['rendering.rgb.sigma'], gamma=params['rendering.rgb.gamma'])
         prd_image = prd_image[...,:3]
-        if(images == None):
-            images = prd_image.unsqueeze(0)
-        else:
-            images = torch.cat((images, prd_image.unsqueeze(0)), dim=0)
-        # images are saved to out as a png 
-        new_L0 = params['rendering.rgb.L0']/prd_image.max()
-        img = (prd_image/prd_image.max()*((256**2)-1)).cpu().numpy().astype(np.uint16)[0]
+        images.append(prd_image)
+        max_val = max(max_val,prd_image.max())
+
+    new_L0 = params['rendering.rgb.L0']/max_val
+    for i in range(n_images):
+    # images are saved to out as a png    
+        img = (images[i]/max_val*((256**2)-1)).cpu().numpy().astype(np.uint16)[0]
         # writer.append_data(img)
         cv2.imwrite(f"./data/dataset/render_{i:02}.png", img)
 
